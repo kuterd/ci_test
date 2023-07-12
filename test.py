b2de61ce08bf3683b5b713ca04b90462b3bc3196
Kuter Dinel <kuterdinel@gmail.com>
2023-07-12 20:47:16 +0300

    break test for testing

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ class TestSimple(unittest.TestCase):
     def test_addition(self):
         return self.assertEqual(123 + 123, 246)
     def test_broken(self):
-        return self.assertEqual(0, 0)
+        return self.assertEqual(0, 1)
 
 if __name__ == '__main__':
     unittest.main()
