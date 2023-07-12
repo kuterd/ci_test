94b0a1daf31a621d05effecd59df811842107bd8
Kuter Dinel <kuterdinel@gmail.com>
2023-07-12 20:37:59 +0300

    add python test

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestSimple(unittest.TestCase):
+    def test_addition(self):
+        return self.assertEqual(123 + 123, 246)
+    def test_broken(self):
+        return self.assertEqual(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
